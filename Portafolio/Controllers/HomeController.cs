e853657975fb574aeec756b444b83146854cd465
fanny00 <fannycamposa@gmail.com>
2023-02-28 16:47:45 -0300

    HTTP POST - Posteando el formulario

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -36,7 +36,14 @@ namespace Portafolio.Controllers
             return View(proyectos);
         }
 
+        [HttpGet]
         public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
         {
             return View();
         }
