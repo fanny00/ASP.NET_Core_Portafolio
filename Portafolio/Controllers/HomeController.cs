371cfe0866cef6b27c799501fbad7f3fbad6887a
fanny00 <fannycamposa@gmail.com>
2023-02-28 14:50:59 -0300

    Proveedores de Configuracion

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -10,23 +10,26 @@ namespace Portafolio.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
-        
+        private readonly IConfiguration configuration;
 
         public HomeController(ILogger<HomeController> logger, 
-            IRepositorioProyectos repositorioProyectos)
+            IRepositorioProyectos repositorioProyectos,
+            IConfiguration configuration)
         {
             _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            var apellido = configuration.GetValue<string>("Apellido");
             _logger.LogTrace("Este es un mensaje de trace");
             _logger.LogDebug("Este es un mensaje de debug");
             _logger.LogInformation("Este es un mensaje de información");
             _logger.LogWarning("Este es un mensaje de warning");
             _logger.LogError("Este es un mensaje de error");
-            _logger.LogCritical("Este es un mensaje de critical");
+            _logger.LogCritical("Este es un mensaje de critical " + apellido);
 
             var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             
